5752f9bf6ededeccbc90bd5973b586fc37e50f8c
OmarDer <odervisevi1@etf.unsa.ba>
2017-04-12 19:22:13 +0200

    Komunikacija mikroservisa - Pocetak

--- Backend/Proizvodi modul/FitSit-Proizvodi/src/main/java/ba/fitandsit/controllers/ProgramiController.java
+++ Backend/Proizvodi modul/FitSit-Proizvodi/src/main/java/ba/fitandsit/controllers/ProgramiController.java
@@ -1,18 +1,34 @@
 package ba.fitandsit.controllers;
 import ba.fitandsit.wrappers.*;
+
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.inject.spi.Message;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import ba.fitandsit.repository.ProgramiRepository;
 import ba.fitandsit.services.ProgramiService;
@@ -84,6 +100,35 @@ public class ProgramiController {
 	{
 		Long Id=Long.parseLong(id);
 		return ps.obrisiProizvodeIzPrograma(Id,p.getId());
+		
+	}
+	
+	
+	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	public void setDiscoveryClient(DiscoveryClient discoveryClient) {
+		this.discoveryClient = discoveryClient;
+	}
+
+	public String serviceUrl() {
+	    List<ServiceInstance> list = discoveryClient.getInstances("KORISNICI");
+	    if (list != null && list.size() > 0 ) {
+	        return list.get(0).getUri().toString();
+	    }
+	    return null;
+	}
+	
+	
+	@RequestMapping(value="/{id}/korisnik",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	public String dajKorisnikaZaProgram(@PathVariable Long id) 
+	{
+		RestTemplate rt=new RestTemplate();
+		JsonWrapperProgrami p=ps.vratiProgramPoID(id);
+		String url=serviceUrl();
+		
+		return rt.getForObject(url+"/korisnici/"+p.getProgram().getAutorID(), String.class);
+		
 		
 	}
 }
